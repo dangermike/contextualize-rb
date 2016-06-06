0b353b6925c7ca635a1eb60281296cc1895ad919
Mike Hurwitz <dangermike@shutterstock.com>
2016-06-06 11:35:55 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Contextualize
+Super sleazy little script to extract a common expression and prepend it to all following lines. This makes lists with headers easier to filter with `grep` or similar. 
+
+```
+Usage: contextualize [options]
+    -c, --context REGEX              Regex to identify context lines
+```
---
+++ contextualize.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: contextualize [options]"
+  opts.on('-c', '--context REGEX', 'Regex to identify context lines') { |v| options[:regex] = v }
+end.parse!
+
+puts options
+
+rx = /#{options[:regex]}/
+
+context = ""
+
+ARGF.each_with_index do |line, idx|
+	m = rx.match(line)
+	context = m[0] if m
+    print context, ": ", line
+end
